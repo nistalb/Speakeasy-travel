366361812cbfc162b3eb7e46fe93f6f63205497b
nistalb <brent.daddy@gmail.com>
2020-12-17 17:07:44 -0800

    added paths in all controllers for routes

--- controllers/trip.js
+++ controllers/trip.js
@@ -10,20 +10,38 @@ const router = express.Router();
 
 //index
 router.get("/", (req, res) => {
-    res.send("we are on the index page!")
+    res.send("we are on the trip index page!")
 });
 
 //new
+router.get("/new", (req, res) => {
+    res.send("You are on the new trip page")
+});
 
 //create
+router.post("/new", (req, res) => {
+    
+});
 
 //show
+router.get("/:id", (req, res) => {
+    res.send("you are on the trip show page")
+});
 
 //edit
+router.get("/:id/edit", (req, res) =>{
+
+});
 
 //update
+router.push("/:id", (req, res) => {
+
+});
 
 //delete
+router.delete("/:id", (req, res) => {
+
+});
 
 //export router
 module.exports = router;
--- controllers/user.js
+++ controllers/user.js
@@ -9,14 +9,24 @@ const router = express.Router();
 /* Create routes */
 
 //register
-router.get("/register", (req, res) => {
+router.post("/register", (req, res) => {
     res.send("On the register page")
 });
 
 //login
+router.get("/login", (req, res) => {
+
+});
+
+//verify login information
+router.post("/login", (req, res) => {
+
+})
 
 //delete
+router.delete("/logout", (req, res) => {
 
+});
 
 
 //export router
