5ba3fb20536db8a0bcb599db1c3d5fc4ee5ba03c
nistalb <brent.daddy@gmail.com>
2020-12-17 21:51:47 -0800

    installed bcrypt and created register route in auth.js in controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,41 @@
 //require express
 const express = require("express");
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 //require the data
-//const db = require("../models");
+const db = require("../models");
 
 /* Create routes */
 
 //register
-router.post("/register", (req, res) => {
-    res.send("On the register page")
+router.get("/register", (req, res) => {
+    res.render("auth/register");
+})
+
+router.post("/register", async (req, res) => {
+    
+    try{
+
+        const foundUser = await db.User.findOne( {email: req.body.email });
+
+        if (foundUser) return res.redirect("/login");
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        const newUser = await db.User.create(req.body);
+
+        return res.redirect("/login");
+
+    } catch (err) {
+        return res.send(err);
+    };
 });
 
 //login
 router.get("/login", (req, res) => {
-
+    res.send("on the login page")
 });
 
 //verify login information
